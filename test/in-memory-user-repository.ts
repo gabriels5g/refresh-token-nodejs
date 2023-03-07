194b35507aab3095ebf049c838fe66192c9957c3
gabriels5g <gabrielspxls@gmail.com>
2023-03-07 19:03:50 -0300

    class to simulate in-memory database to perform tests

---
+++ test/in-memory-user-repository.ts
@@ -0,0 +1,20 @@
+import { User } from "../src/application/entities/user";
+import { UserRepository } from "../src/application/repositories/user-repository";
+
+export class InMemoryUserRepository implements UserRepository {
+  public users: User[] = [];
+
+  async create(user: User): Promise<void> {
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const foundUser = this.users.find((user) => user.email === email);
+
+    if (foundUser) {
+      return foundUser;
+    } else {
+      return null;
+    }
+  }
+}
