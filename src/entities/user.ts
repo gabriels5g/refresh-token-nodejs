40ec2c6c92a7655f693c35455eaf9499e76be613
gabriels5g <gabrielspxls@gmail.com>
2023-03-06 23:13:24 -0300

    create user entity

---
+++ src/entities/user.ts
@@ -0,0 +1,31 @@
+interface UserProps {
+  name: string
+  userName: string
+  email: string
+  password: string
+}
+
+
+export class User {
+  private _id: string;
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public name(): string {
+    return this.props.name;
+  }
+
+  public email(): string {
+    return this.props.email;
+  }
+
+  public password(): string {
+    return this.props.password;
+  }
+  
+  constructor(private props: UserProps) {
+    this._id = crypto.randomUUID()
+  }
+}
