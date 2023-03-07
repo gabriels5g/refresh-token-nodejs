2d0522bb2923463f6b782786cafa37edc826b740
gabriels5g <gabrielspxls@gmail.com>
2023-03-06 23:23:01 -0300

    first test by checking if sut is an instance of User

---
+++ src/entities/user.spec.ts
@@ -0,0 +1,15 @@
+import { describe, it, expect } from "vitest"
+import { User } from "./user"
+
+describe('User', () => {
+  it('should create a user', () => {
+    const sut = new User({
+      name: "John",
+      userName: "John123",
+      email: "example@example.com",
+      password: "example-password"
+    })
+
+    expect(sut).toBeInstanceOf(User)
+  })
+})
--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,3 +1,5 @@
+import crypto from "crypto";
+
 interface UserProps {
   name: string
   userName: string
