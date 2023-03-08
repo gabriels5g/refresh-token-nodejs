9fa3480230a632b8b964d172912f04b40a002df3
gabriels5g <gabrielspxls@gmail.com>
2023-03-08 01:50:14 -0300

    test: return error 400 if email format is invalid

--- src/application/use-cases/sign-up/sign-up.spec.ts
+++ src/application/use-cases/sign-up/sign-up.spec.ts
@@ -1,15 +1,16 @@
 import { describe, it, expect } from "vitest";
 import { InMemoryUserRepository } from "../../../../test/in-memory-user-repository";
+
 import { SingUp } from "./sign-up";
 
 describe("SignUp", () => {
-  it("should create a user", async () => {
+  it("  should return error 400 if email already exists", async () => {
     const userRepository = new InMemoryUserRepository();
     const createUser = new SingUp(userRepository);
     await createUser.execute({
       name: "user1",
       userName: "user2",
-      email: "user@example.com",
+      email: "email@example.com",
       password: "Ga271200!",
     });
 
@@ -18,9 +19,23 @@ describe("SignUp", () => {
       createUser.execute({
         name: "user2",
         userName: "user1",
-        email: "user@example.com",
+        email: "email@example.com",
         password: "Ga271200!",
       })
     ).rejects.toThrow("email already registered");
+  });
+
+  it("should return error 400 if email format is invalid", async () => {
+    const userRepository = new InMemoryUserRepository();
+    const createUser = new SingUp(userRepository);
+
+    await expect(
+      createUser.execute({
+        name: "user1",
+        userName: "userName",
+        email: "example-email-invalid",
+        password: "example-password",
+      })
+    ).rejects.toThrow("email format is invalid");
   });
 });
