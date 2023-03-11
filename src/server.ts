04ccb7e6577f022f25b4f5abf7a27ab0f89132d5
gabriels5g <gabrielspxls@gmail.com>
2023-03-11 02:28:50 -0300

    route refresh token

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { SignUpController } from "../src/controller/sign-up-controller";
+import { refreshTokenUserController } from "./controller/refresh-token-user-controlle";
 import { SignInController } from "./controller/sign-in-controller";
 
 const app = express();
@@ -7,8 +8,10 @@ app.use(express.json());
 
 const createUser = new SignUpController();
 const authUser = new SignInController();
-app.post("/signup", createUser.execute);
+const refreshToken = new refreshTokenUserController();
 
+app.post("/users", createUser.execute);
 app.post("/login", authUser.handle);
+app.post("/refresh-token", refreshToken.handle);
 
 app.listen({ port: 3000 });
